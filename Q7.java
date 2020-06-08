9a4d96024ff63dad1833d148d39c7d341d145706
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-08 21:38:18 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,3 +1,5 @@
+/*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?*/
 public class Q7 {
 	public static boolean checkprime(int n) {
 		for(int i=2;i<=Math.sqrt(n);i++) {
@@ -18,3 +20,4 @@ public class Q7 {
 		System.out.println(prime-1);
 	}
 }
+//Output104743
