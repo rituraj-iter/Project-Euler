b97ba9e45defa3c181f1693b8309e22dc5def82c
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-08 21:41:31 +0530

    Update Q10.java

--- Q10.java
+++ Q10.java
@@ -1,3 +1,5 @@
+/*The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.*/
 public class Q10 {
 	public static boolean checkprime(int n) {
 		for(int i=2;i<=Math.sqrt(n);i++) {
@@ -19,3 +21,4 @@ public class Q10 {
 		 System.out.println(s);
 	 }
 }
+//Output142913828922
