0f9c6876f7a34f824525a5c2ee1040c5b8472e3f
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-08 22:30:32 +0530

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -6,11 +6,7 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.*/
 public class Q6{
 	public static void main (String args[]){
-		int a=0,b=0;
-		for (int i=1;i<=100;i++){
-			a=a+(i*i);
-			b=b+i;
-			}
+		int a=(100*101*201)/6,b=(100*101)/2;
 		System.out.print(((b*b)-a));
 		}
 }
