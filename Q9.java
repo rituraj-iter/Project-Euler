cc0fa11987bba91e553bda0c26c76048090c70a9
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-08 21:40:44 +0530

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -1,3 +1,8 @@
+/*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.*/
 public class Q9 {
 public static void main(String[] args) {
         int a,b,c;
@@ -16,3 +21,4 @@ public static void main(String[] args) {
         System.out.println(res);
         }
 }
+//Output31875000
