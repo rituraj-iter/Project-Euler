227357eaacaeb8031d62b5ca5e5c1a34ece24c00
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-08 21:33:19 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,3 +1,5 @@
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?*/
 public class Q3 {
 public static double prime(double a) {
 	double n;
@@ -15,3 +17,4 @@ public static void main(String args[]) {
 	System.out.println((int)prime(600851475143d));
 }
 }
+//Output6857
