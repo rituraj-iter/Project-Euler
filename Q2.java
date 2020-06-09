9ba562460b64921a6068ca0821689447adf53e62
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-09 05:50:41 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -2,22 +2,19 @@
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.*/
 public class Q2 {
-public static int evenfibo() {
-	int m=0;
-	int n=1;
-	int sum=0;
-	int temp=0;
-	while(n<4*Math.pow(10, 6)){
-		if(n%2==0) {
-		sum=sum+n;}
-		temp=n;
-		n=m+n;
-		m=temp;
-	}
-	return sum;
-}
-public static void main(String args[]) {
-	System.out.println(evenfibo());
-}
+	public static void main(String args[]) {
+		int m=0;
+		int n=1;
+		int sum=0;
+		int temp=0;
+		while(n<4*Math.pow(10, 6)){
+			if(n%2==0) {
+			sum=sum+n;}
+			temp=n;
+			n=m+n;
+			m=temp;
+		}
+		System.out.println(sum);
+		}
 }
 //Output4613732
