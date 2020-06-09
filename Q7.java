6d14e5169285cc90950cb72a41ba965486671f90
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-09 06:29:08 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -2,22 +2,22 @@
 What is the 10 001st prime number?*/
 public class Q7 {
 	public static boolean checkprime(int n) {
-		for(int i=2;i<=Math.sqrt(n);i++) {
+		for(int i=3;i<=Math.sqrt(n);i+=2) {
 			if(n%i==0)
 				return false;
 		}
 		return true;
 	}
 	public static void main(String args[]) {
-		int n=0;
-		int prime=2;
-		while(n<10001) {
+		int n=2;
+		int prime=3;
+		while(n<=10001) {
 			if(checkprime(prime)) {
 				n++;
 			}
-			prime++;
+			prime+=2;
 		}
-		System.out.println(prime-1);
+		System.out.println(prime-2);
 	}
 }
 //Output104743
